4d20c7b1936bf7e5834c13b707d5b65723b03540
Hien Ho Developer <hien.ho@nextfunc.com>
2019-11-21 20:45:17 +0700

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Hien Ho Developer on 11/21/19.
+//  Copyright © 2019 Hien Ho Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
